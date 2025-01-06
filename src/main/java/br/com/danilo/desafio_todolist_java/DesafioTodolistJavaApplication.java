487e5a40bf666d89503c6bbded81ba455155a9b4
danilo <daniloaraujomota@gmail.com>
2025-01-06 10:16:11 -0300

    first commit

---
+++ src/main/java/br/com/danilo/desafio_todolist_java/DesafioTodolistJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.danilo.desafio_todolist_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTodolistJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTodolistJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/danilo/desafio_todolist_java/DesafioTodolistJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danilo.desafio_todolist_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodolistJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
